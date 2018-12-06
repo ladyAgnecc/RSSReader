b4a4bcc593c54eb8a86bea0fb3fa2404fbf6f63a
Jenny Rykun <>
2018-12-06 18:27:19 +0200

    initial commit

---
+++ RSSReader/RSSReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RSSReader
+//
+//  Created by Jenny Rykun on 12/6/18.
+//  Copyright © 2018 Jenny Rykun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
