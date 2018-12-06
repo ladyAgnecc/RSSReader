7ca1627ad8b143b39af61191b724cc777fb9f7fa
Jenny Rykun <>
2018-12-06 20:13:13 +0200

    added tab bar controoler

---
+++ RSSReader/RSSReader/Flows/News/NewsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NewsViewController.swift
+//  RSSReader
+//
+//  Created by Jenny Rykun on 12/6/18.
+//  Copyright © 2018 Jenny Rykun. All rights reserved.
+//
+
+import UIKit
+
+class NewsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
